fb1503aa182baf5f50782d972c8fcb2adbd2cbe3
NuBzIiM <breendhonh@gmail.com>
2023-06-29 23:44:30 -0400

    Spinner (Carregando)

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -12,7 +12,7 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category']
 
   //coursesService: CoursesService
@@ -20,7 +20,7 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService: CoursesService) {
     //this.courses = [];
     //this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
     }
 
   ngOnInit(): void {
