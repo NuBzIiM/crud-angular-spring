303d115462a1014e9948e6613df553c56746170c
NuBzIiM <breendhonh@gmail.com>
2023-06-30 11:54:37 -0400

    API HTTP GET

--- crud-spring/src/main/java/com/hugo/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/hugo/crudspring/CrudSpringApplication.java
@@ -1,13 +1,30 @@
 package com.hugo.crudspring;
 
+import com.hugo.crudspring.model.Course;
+import com.hugo.crudspring.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CrudSpringApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner initDatasave(CourseRepository courseRepository) {
+        return args -> {
+            courseRepository.deleteAll();
+
+            Course c = new Course();
+            c.setName("Angular com Spring");
+            c.setCategory("front-end");
+
+            courseRepository.save(c);
+        };
+    }
 }
---
+++ crud-spring/src/main/java/com/hugo/crudspring/model/Course.java
@@ -0,0 +1,22 @@
+package com.hugo.crudspring.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String category;
+}
