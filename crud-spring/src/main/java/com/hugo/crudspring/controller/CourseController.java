58ede810ed57926272d4842ae57d60ded6ae407a
Breendhon Hugo <54628603+NuBzIiM@users.noreply.github.com>
2023-07-03 15:57:01 -0400

    API Spring: Buscar por ID

--- crud-spring/src/main/java/com/hugo/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/hugo/crudspring/controller/CourseController.java
@@ -5,6 +5,7 @@ import com.hugo.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,13 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
