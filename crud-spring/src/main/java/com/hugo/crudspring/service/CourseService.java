47857cc0008da9ec464b3da4b88bdf54db810202
Breendhon Hugo <54628603+NuBzIiM@users.noreply.github.com>
2023-07-06 15:12:46 -0400

    Tratando Exceções com Controller Advice

--- crud-spring/src/main/java/com/hugo/crudspring/service/CourseService.java
+++ crud-spring/src/main/java/com/hugo/crudspring/service/CourseService.java
@@ -1,27 +1,23 @@
 package com.hugo.crudspring.service;
 
+import com.hugo.crudspring.exception.RecordNotFoundException;
 import com.hugo.crudspring.model.Course;
 import com.hugo.crudspring.repository.CourseRepository;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import java.util.List;
-import java.util.Optional;
 
 @Validated
 @Service
 public class CourseService {
 
     private final CourseRepository courseRepository;
-
 
     public CourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
@@ -31,29 +27,26 @@ public class CourseService {
         return courseRepository.findAll();
     }
 
-    public Optional<Course> findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id);
+    public Course findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id));
     }
 
     public Course create(@Valid Course course) {
         return courseRepository.save(course);
     }
 
-    public Optional<Course> update( @NotNull @Positive Long id, @Valid Course course) {
+    public Course update( @NotNull @Positive Long id, @Valid Course course) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
                     recordFound.setName(course.getName());
                     recordFound.setCategory((course.getCategory()));
                     return courseRepository.save(recordFound);
-                });
+                }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public boolean delete(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return true;
-                })
-                .orElse(false);
+    public void delete(@PathVariable @NotNull @Positive Long id) {
+        courseRepository.delete(courseRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id)));
     }
 }
